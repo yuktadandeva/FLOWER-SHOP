deeaa62a707bb173c87cb6d170ab39e9d1d64874
yuktadandeva <153160647+yuktadandeva@users.noreply.github.com>
2024-07-09 01:28:11 +0530

    Add files via upload

---
+++ FLOWER STORE/cart-service.js
@@ -0,0 +1,26 @@
+const cartOperations = {
+
+    flowers: [],
+    addToCart(){
+
+        const phool = this.flowers.find(phool => phool.productId == flower-id);
+        phool.isInCart = 'true';
+
+        console.log(this.flowers);
+
+    },
+
+    removeFromCart(){
+
+    },
+
+    viewAll(){
+
+    },
+
+    total(){
+
+    }
+}
+
+export default cartOperations;
